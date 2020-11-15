8990fe1627a32e87e514a33092f12641c5382bb3
Novojit Das <novojitdas@gmail.com>
2020-11-15 23:18:14 +0600

    Create list02.py

---
+++ list02.py
@@ -0,0 +1,9 @@
+# Concatenate two lists index-wise
+
+list1 = ["M", "na", "i", "Ke"]
+list2 = ["y", "me", "s", "lly"]
+lists = []
+for number in range(0, 4):
+    final = list1[number] + list2[number]
+    lists = final[]
+print(lists)
